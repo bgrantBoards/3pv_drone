3029b666abe8813543aa6f552968aed62c913319
Benjamin Grant <78234854+bgrantBoards@users.noreply.github.com>
2023-10-31 11:00:04 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = '3pv_drone'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ben',
+    maintainer_email='78234854+bgrantBoards@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "test_node = 3pv_drone.test_node:main",
+        ],
+    },
+)
