714ed78644e06cc8976bdf5028960b86708eb2c2
Benjamin Grant <78234854+bgrantBoards@users.noreply.github.com>
2023-11-28 00:16:33 -0500

    tried fixing PID twitch by low pass filtering cmd vel. sorta worked

--- 3pv_drone/crazyflie_motion_control.py
+++ 3pv_drone/crazyflie_motion_control.py
@@ -32,12 +32,19 @@ class CrazyfliePositionController(Node):
         )
         
         # pid controllers
-        self.pid_x = PID(Kp = -1, Ki = -0.1, Kd = -0.001, setpoint = 2)
+        self.pid_x = PID(
+            Kp = -0.5,
+            Ki = 0.0,
+            # Kd = -0.01,
+            setpoint = 2,
+            output_limits = (-3, 3)
+        )
 
         # current state
         self.tag_pose_relative:Pose = None
-        self.filter_size = 100
+        self.filter_size = 50
         self.x_filter = list() # for implementing rolling average noise filter
+        self.x_vel_filter = list()
     
     def recieve_relative_tag_pose(self, msg:Pose) -> None:
         self.tag_pose_relative = msg
@@ -50,9 +57,15 @@ class CrazyfliePositionController(Node):
         if self.tag_pose_relative:
             cmd_vel_msg = Twist() # instantiate empty Twist msg
 
-            # populate Twist msg with PID computed control velocities
-            cmd_vel_msg.linear.x = self.pid_x(sum(self.x_filter)/self.filter_size)
+            # calculate (filtered) cmd velocity
+            while len(self.x_vel_filter) < self.filter_size + 1:
+                x_vel = self.pid_x(sum(self.x_filter)/self.filter_size)
+                self.x_vel_filter.append(x_vel)
+            self.x_vel_filter.pop(0)
+            
 
+            # populate Twist msg with PID computed control velocities
+            cmd_vel_msg.linear.x = float(sum(self.x_vel_filter)/len(self.x_vel_filter))
             # send Twist msg
             self.cmd_vel_pub.publish(cmd_vel_msg)
         
